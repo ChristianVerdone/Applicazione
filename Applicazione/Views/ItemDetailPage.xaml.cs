0ed42f6dfd45c4795c61816bef9a0a9b7c46beb3
Christian Verdone <christian_ve98@hotmail.it>
2022-06-03 15:36:50 +0200

    Creation app

---
+++ Applicazione/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Applicazione.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Applicazione.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Applicazione/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using Applicazione.Models;
+using Applicazione.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Applicazione.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
